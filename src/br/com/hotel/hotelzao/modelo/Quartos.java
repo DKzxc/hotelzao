7200de2bc5b56b5960b64d137478f1473f8c9d2c
David Rivelli <davidcrudelirivelli@gmail.com>
2021-10-26 23:17:57 -0300

    commit3 26/10/21

--- src/br/com/hotel/hotelzao/modelo/Quartos.java
+++ src/br/com/hotel/hotelzao/modelo/Quartos.java
@@ -5,9 +5,5 @@ public class Quartos {
     private String numero;
     private double valor;
 
-    //"abra cadabra alado"
-   // "oiiii "
-    //alooooooooooooooooooowww
-    //oh yes
-    //oh no
+
 }
