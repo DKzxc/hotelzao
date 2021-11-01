aa22161c223f20fedc07ea38d9c3feeacbf7a076
Filipe Bueno <filipeebueno@gmail.com>
2021-11-01 12:37:04 -0300

    Criação do método mostrarInformacoes para simplificar a impressão dos clientes e criação do método creat para simplificar a criação de um novo Guest

--- src/br/com/hotel/hotelzao/test/TestGuest.java
+++ src/br/com/hotel/hotelzao/test/TestGuest.java
@@ -7,18 +7,28 @@ public class TestGuest {
 
     public static void main(String[] args) {
 
-        Adress endereco = new Adress("Rua jiraya",400,"Mezopotamia","Acre");
-        Guest g1 = new Guest("Jao","111.111.111-11","pedrerao",29,endereco);
+        Guest g1 = Guest.create("João", "111.111.111-11", "Rua jiraya");
 
-        System.out.println(g1.getFullName());
-        System.out.println(g1.getAge());
-        System.out.println(g1.getOccupation());
-        System.out.println(g1.getDocument());
-        System.out.println("---------------------");
-        System.out.println(g1.getAdress().getStreet());
-        System.out.println(g1.getAdress().getNumber());
-        System.out.println(g1.getAdress().getCity());
-        System.out.println(g1.getAdress().getState());
+//        System.out.println(g1.getFullName());
+//        System.out.println(g1.getAge());
+//        System.out.println(g1.getOccupation());
+//        System.out.println(g1.getDocument());
+//        System.out.println("---------------------");
+//        System.out.println(g1.getAdress().getStreet());
+//        System.out.println(g1.getAdress().getNumber());
+//        System.out.println(g1.getAdress().getCity());
+//        System.out.println(g1.getAdress().getState());
+
+        g1.mostrarInformacoes();
+
+        Guest g2 = Guest.create("Tadeu", "111.111.111-11", "Rua jiraya");
+
+        g2.mostrarInformacoes();
+
+
+
     }
+
+
 
 }
